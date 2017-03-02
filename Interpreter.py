15436dabad1a9436e1084e0665ec8842fe02fdab
Luiz Lima <lf.200593@gmail.com>
2017-03-02 09:30:01 -0300

    Adicionando arquivos iniciais

---
+++ Interpreter.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 02 08:20:46 2017
+
+@author: aluno
+"""
+program_counter = -1
+current_instruction = -1
+instruction_type = -1
+run_bit = True
+
+def get_instruction_type(current_instruction):
+    
+
+def find_data(current_instruction, instruction_type):
+    data = open("data.txt","r+")
+    
+    data.close()
+
+def execute(instruction_type, data):
+    
+
+def interpret(program, starting_address):
+    program_counter = starting_address
+    while run_bit:
+        current_instruction = program[program_counter]
+        program_counter += 1
+        instruction_type = get_instruction_type(current_instruction)
+        data_location = find_data(current_instruction, instruction_type)
+        if data_location >= 0:
+            data = program[data_location]
+        execute(instruction_type, data)
---
+++ OPCodes.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 02 09:25:37 2017
+
+@author: aluno
+"""
+
+codes = {0:'store', 1:'load', 2:'move', 3:'add', 4:'sub'}
