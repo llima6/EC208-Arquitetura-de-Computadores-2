76846ad53736cb5328df8836c5cec5d17c17ef9d
Luiz Lima <lf.200593@gmail.com>
2017-03-13 19:10:00 -0300

    Update opcodes.py

--- opcodes.py
+++ opcodes.py
@@ -4,8 +4,11 @@ Created on Thu Mar 02 09:25:37 2017
 
 @author: aluno
 """
-
 instructions = {'0000':0, '0001':1, '0010':2, '0011':3, '0100':4, '0101':5, '0110':6}
+
+def type(instruction):
+  return instructions[instruction]
+
 """
 0 = store
 1 = load
