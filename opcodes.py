be2fb1f00c16756352768c8a66a51bab12b506ee
Luiz Lima <lf.200593@gmail.com>
2017-03-13 18:30:22 -0300

    Update opcodes.py

--- opcodes.py
+++ opcodes.py
@@ -5,4 +5,14 @@ Created on Thu Mar 02 09:25:37 2017
 @author: aluno
 """
 
-instructions = {0b000:'store', 0b0001:'load', 0b0010:'move', 0b0011:'add', 0b0100:'sub', 0b0101:'prod', 0b0110:'div'}
+instructions = {'0000':0, '0001':1, '0010':2, '0011':3, '0100':4, '0101':5, '0110':6}
+"""
+0 = store
+1 = load
+2 = move
+3 = add
+4 = sub
+5 = prod
+6 = div
+"""
+
